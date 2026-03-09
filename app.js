d731668a61a450c5df608b7bee02da1cc9273222
Daba Gueye <11gueyedaba10@gmail.com>
2026-03-09 15:11:48 +0100

    premier commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const boutonMenu = document.querySelector('#bouton-menu');
+const menu = document.querySelector('#menu');
+
+boutonMenu.addEventListener('click', function() {
+    menu.classList.toggle("hidden");
+
+    const iconMenu = document.querySelector('#bouton-menu i');
+
+    if (menu.classList.contains("hidden")) {
+        iconMenu.className = "fa-solid fa-bars fa-3x";
+    } else {
+        iconMenu.className = "fa-solid fa-xmark fa-3x";
+    }
+});
