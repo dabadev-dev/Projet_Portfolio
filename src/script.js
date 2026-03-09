00d994cdb867a775770f508cb072ff3077ae631c
Daba Gueye <11gueyedaba10@gmail.com>
2026-03-09 14:29:34 +0100

    premier commit

---
+++ src/script.js
@@ -0,0 +1,27 @@
+// const boutonMenu = document.querySelector('#bouton-menu');
+// const menu = document.querySelector('#menu');
+// boutonMenu.addEventListener('click', function() {
+//     const menuEstActive = menu.classList.toggle("active");
+//     const iconMenu = document.querySelector('#bouton-menu i')
+//     if (menuEstActive) {
+//         iconMenu.className = "fa-solid fa-xmark fa-3x";
+//     } else {
+//         iconMenu.className = "fa-solid fa-bars fa-3x";
+//     }
+// });
+
+
+const boutonMenu = document.querySelector('#bouton-menu');
+const menu = document.querySelector('#menu');
+
+boutonMenu.addEventListener('click', function() {
+    menu.classList.toggle("hidden");
+
+    const iconMenu = document.querySelector('#bouton-menu i');
+
+    if (menu.classList.contains("hidden")) {
+        iconMenu.className = "fa-solid fa-bars fa-3x";
+    } else {
+        iconMenu.className = "fa-solid fa-xmark fa-3x";
+    }
+});
